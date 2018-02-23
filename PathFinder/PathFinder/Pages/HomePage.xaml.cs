88d437b5e8cb67eb8ce3f28f8cef0fe99c1a5479
Manoj Punekar <mpunekar@dminc.com>
2018-02-23 18:12:51 +0530

    updated code

--- PathFinder/PathFinder/Pages/HomePage.xaml.cs
+++ PathFinder/PathFinder/Pages/HomePage.xaml.cs
@@ -12,9 +12,23 @@ namespace PathFinder.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
-		public HomePage ()
+        Dictionary<string, string> SubCategory = new Dictionary<string, string>
+        {
+            { "OPD", "Hospital" }, { "Casualty", "Hospital" },
+            { "Emergency", "Hospital" }, { "Cardiologist", "Hospital" },
+            { "Ortho", "Hospital" }, { "Neuro", "Hospital" },
+            { "Reception", "Hospital" }, { "Enquiry", "Hospital" },
+            { "I.C.U", "Hospital" }, { "Operation Theatre", "Hospital" }
+       
+        };
+        public HomePage ()
 		{
 			InitializeComponent();
 		}
+
+        private void PickerList_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            //PickerLabel.Text = PickerList.Items[PickerList.SelectedIndex];
+        }
     }
 }
