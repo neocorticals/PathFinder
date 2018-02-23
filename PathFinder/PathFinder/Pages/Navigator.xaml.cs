21d7e703e1f3e4906f45854f1d9de3246be6bae9
Manoj Punekar <mpunekar@dminc.com>
2018-02-23 18:12:41 +0530

    updated code

---
+++ PathFinder/PathFinder/Pages/Navigator.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PathFinder.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Navigator : ContentPage
+	{
+		public Navigator ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
